b73233f9f59cde03560c7c334c83743390578a21
krishna8429 <140785121+krishna8429@users.noreply.github.com>
2023-08-20 01:27:56 -0700

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -1,7 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const calculatedBMR = localStorage.getItem('calculatedBMR');
-    if (calculatedBMR) {
-      document.getElementById('calculatedBMR').textContent = calculatedBMR;
-    }
-  });
-  
+// Assume this function calculates the BMR and updates the displayed value
+function calculateBMR() {
+  // Calculate BMR logic here
+  // ...
+
+  // Update the displayed BMR value
+  const calculatedBMR = 1500; // Replace with actual calculated BMR
+  document.getElementById("calculatedBMR").textContent = calculatedBMR;
+}
+
+// Function to show the goal options when the button is clicked
+function showGoalOptions() {
+  const goalOptions = ["Weight Gain", "Muscle Gain", "Maintain Weight"];
+  const goalOptionsString = goalOptions.map(option => `<option>${option}</option>`).join("");
+
+  const selectElement = document.createElement("select");
+  selectElement.innerHTML = goalOptionsString;
+
+  document.querySelector(".container").appendChild(selectElement);
+}
+
+document.getElementById("enterGoalButton").addEventListener("click", showGoalOptions);
