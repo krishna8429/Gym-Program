7fc64da7d1357d2424a1b5bb256b0cda8197fa0e
krishna8429 <140785121+krishna8429@users.noreply.github.com>
2023-08-19 22:39:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const calculateButton = document.getElementById("calculate-button");
-    const outputDiv = document.getElementById("output");
-    const workoutPlan = document.getElementById("workout-plan");
-    const dietPlan = document.getElementById("diet-plan");
-
-    calculateButton.addEventListener("click", () => {
-        const height = parseFloat(document.getElementById("height").value);
-        const weight = parseFloat(document.getElementById("weight").value);
-        const goal = document.getElementById("goal").value;
-
-        // Example workout and diet plans
-        const workoutPlans = {
-            weight_loss: "Cardio exercises and low-calorie diet.",
-            muscle_gain: "Strength training and high-protein diet.",
-            maintain: "Balanced workout and diet to maintain current state."
-        };
-
-        const dietPlans = {
-            weight_loss: "Reduce calorie intake and focus on whole foods.",
-            muscle_gain: "Increase protein and calorie intake for muscle growth.",
-            maintain: "Maintain a balanced diet with proper portion control."
-        };
-
-        workoutPlan.textContent = workoutPlans[goal];
-        dietPlan.textContent = dietPlans[goal];
-
-        outputDiv.classList.remove("hidden");
-    });
-});
+function calculateBMR() {
+    const age = parseFloat(document.getElementById('age').value);
+    const height = parseFloat(document.getElementById('height').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+    const gender = document.getElementById('gender').value;
+  
+    let bmr;
+  
+    if (gender === 'male') {
+      bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+    } else if (gender === 'female') {
+      bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+    }
+  
+    // Store data in localStorage or pass it to the next page using query parameters
+    // For simplicity, let's just alert the calculated BMR for now
+    alert(`Your Basal Metabolic Rate (BMR) is: ${bmr.toFixed(2)}`);
+  }
+  
